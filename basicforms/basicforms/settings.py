87b661c4b167d6f085f7776b5f9b781cfd9be6d3
Dibbajothy Sarker <64429961+Dibbajothy@users.noreply.github.com>
2024-08-24 04:10:39 +0600

    Update settings.py

--- basicforms/basicforms/settings.py
+++ basicforms/basicforms/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*vd$i42fs)(tvtef#p)3kczzbp5xgfahu=)@qf58=-=(!@4uji'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*', '.vercel.app']
 
